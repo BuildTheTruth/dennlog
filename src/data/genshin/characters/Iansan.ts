cce7cfc80d7553614c71903d5f68f65ccf85ff45
BuildTheTruth <dkwk1254@naver.com>
2025-04-19 23:57:01 +0900

    얀사 데이터 추가

--- src/data/genshin/characters/Iansan.ts
+++ src/data/genshin/characters/Iansan.ts
@@ -1,7 +1,8 @@
+import { getArtifactImageURL, getWeaponImageURL } from '@/lib/image/genshin';
+
 const skills: CharacterSkill[] = [
   {
     id: 'mavuika-skill-basic',
-
     type: '기본공격',
     name: '기본 공격',
     max: 1,
@@ -10,16 +11,14 @@ const skills: CharacterSkill[] = [
   },
   {
     id: 'mavuika-skill-elemental-skill',
-
     type: '원소전투스킬',
     name: '원소 전투 스킬',
-    max: 10,
-    min: 8,
+    max: 8,
+    min: 6,
     priority: 2,
   },
   {
     id: 'mavuika-skill-elemental-burst',
-
     type: '원소폭발',
     name: '원소 폭발',
     max: 10,
@@ -31,110 +30,120 @@ const skills: CharacterSkill[] = [
 const stats: CharacterStat[] = [
   {
     id: 'mavuika-stat-basic',
-
     type: '기본',
     fields: {
-      공격력: '1800+',
-      '원소 마스터리': '120~300',
-      성배: '불원피/공%',
+      시계: '원충/공%',
+      성배: '공격력',
+      왕관: '공격력',
+      공격력: '2500~2700',
+      원충: '160%+',
     },
   },
   {
     id: 'mavuika-stat-normal',
-
     type: '준종결',
     fields: {
-      공격력: '1800+',
-      '원소 마스터리': '120~300',
-      성배: '불원피/공%',
+      시계: '공%',
+      성배: '공격력',
+      왕관: '공격력',
+      공격력: '2700~2900',
+      원충: '180%+',
     },
   },
   {
     id: 'mavuika-stat-final',
-
     type: '종결',
     fields: {
-      공격력: '1800+',
-      '원소 마스터리': '120~300',
-      성배: '불원피/공%',
+      시계: '공%',
+      성배: '공격력',
+      왕관: '공격력',
+      공격력: '3000+',
+      원충: '200%+',
     },
   },
 ];
 
 const artifactSets: ArtifactSet[] = [
   {
-    id: 'mavuika-artifact-set-first',
-
-    name: '흑요석 4세트',
-    score: 1,
-    imageURLs: [],
+    id: 'ScrollOfTheHeroOfCinderCity',
+    name: '잿더미 4세트',
+    score: 5,
+    imageURLs: [
+      getArtifactImageURL('ScrollOfTheHeroOfCinderCity/flower'),
+      getArtifactImageURL('ScrollOfTheHeroOfCinderCity/plume'),
+      getArtifactImageURL('ScrollOfTheHeroOfCinderCity/sands'),
+      getArtifactImageURL('ScrollOfTheHeroOfCinderCity/goblet'),
+      getArtifactImageURL('ScrollOfTheHeroOfCinderCity/circlet'),
+    ],
   },
 ];
 
 const weapons: Weapon[] = [
   {
-    id: 'mavuika-weapon-first',
-
-    name: '타오르는 천개의 태양',
-    imageURL: '',
+    id: 'SkywardPride',
+    name: '천공의 마루',
+    imageURL: getWeaponImageURL('SkywardPride'),
     priority: 1,
-    mainStat: '기초 공격력 741',
-    subStat: '치명톼 확률 11.0%',
-  },
-  {
-    id: 'mavuika-weapon-second',
-
-    name: '판정',
-    imageURL: '',
-    priority: 2,
     mainStat: '기초 공격력 674',
-    subStat: '치명타 확률 22.1%',
+    subStat: '원소 충전 효율 36.8%',
   },
   {
-    id: 'mavuika-weapon-third',
-
-    name: '이무기 검',
-    imageURL: '',
+    id: 'TamayurateiNoOhanashi',
+    name: '쉼터의 이야기꾼',
+    imageURL: getWeaponImageURL('TamayurateiNoOhanashi'),
+    priority: 2,
+    mainStat: '기초 공격력 565',
+    subStat: '원소 충전 효율 30.6%',
+  },
+  {
+    id: 'FavoniusLance',
+    name: '페보니우스 장창',
+    imageURL: getWeaponImageURL('FavoniusLance'),
     priority: 3,
     mainStat: '기초 공격력 510',
-    subStat: '치명타 확률 27.6%',
+    subStat: '원소 충전 효율 45.9%',
   },
 ];
 
 const teams: Team[] = [
   {
     id: 'mavuika-team-first',
-
-    name: '마비카 하이퍼캐리',
-    characters: '마비카, 실로닌, 베넷, 실로닌',
-    descriptions: ['마비카 E', '실로닌 (Q)E 평평', '베넷 EQ', '실로닌 EQ 평평'],
-    score: 10,
+    name: '바푸얀한',
+    characters: 'Varesa, Furina, Iansan, Xianyun',
+    descriptions: ['바레사 E', '푸리나 EQ', '얀사 EQ', '한운 EQ', '바레사 E평평x2 Q 반복'],
+    score: 5,
   },
   {
     id: 'mavuika-team-second',
-
-    name: '마비카 오프필드 조합',
-    characters: '마비카, 키니치, 나히다, 실로닌',
-    descriptions: ['마비카 E', '키니치 E(Q)', '나히다 E', '실로닌 EQ 평평'],
-    score: 9,
+    name: '바마얀슈',
+    characters: 'Varesa, Mavuika, Iansan, Chevreuse',
+    descriptions: [
+      '마비카 E',
+      '얀사 Q',
+      '슈브르즈 E',
+      '얀사 E',
+      '바레사 Q E평평x2 Q 반복',
+      '마비카 Q',
+    ],
+    score: 4,
   },
 ];
 
 const breakthroughs: Breakthrough[] = [
   {
     id: 'mavuika-breakthrough-first',
-    name: '1돌: 밤 주인의 계시',
-    description: '밤혼 최대치 및 전의효율 / 공격력 증가 (명함 대비 약 15% 딜 상승)',
+    name: '1돌: 뭐든 시작이 어려운 법',
+    description: '원소 충전 효율 요구량 감소',
   },
   {
     id: 'mavuika-breakthrough-second',
-    name: '2돌: 잿더미의 대가',
-    description: '기초 공격력 및 무장 효과 강화 (명함 대비 약 50% 딜 상승)',
+    name: '2돌: 게으름은 운동의 적!',
+    description: 'EQ 가능, 현재 필드 위 캐릭터의 공격력 30% 증가',
   },
   {
     id: 'mavuika-breakthrough-third',
-    name: '6돌: 인간의 이름 해방',
-    description: '무장 효과 완전 강화 (명함 대비 약 130% 딜 상승)',
+    name: '5돌: 아직 한계가 아니다!',
+    description: '최대 공격력 보너스 690에서 810으로 증가 (공격력 3000+ 필요)',
   },
 ];
 
