55588f3d2b5489f04363987ef6a820368949a88d
BuildTheTruth <dkwk1254@naver.com>
2025-04-24 01:59:00 +0900

    캐릭터 목록 모바일 대응

--- src/app/(routes)/games/genshin/characters/page.tsx
+++ src/app/(routes)/games/genshin/characters/page.tsx
@@ -9,7 +9,7 @@ const GenshinCharactersPage = () => {
   return (
     <div className="flex flex-col gap-4 px-4 md:px-0 w-full max-w-screen-xl mx-auto md:mx-0 md:w-auto">
       <div className="text-black dark:text-white text-lg font-bold">캐릭터 육성법</div>
-      <div className="flex flex-wrap justify-start gap-4">
+      <div className="flex flex-wrap justify-center md:justify-start gap-4">
         {GENSHIN_CHARACTERS.map(character => (
           <div
             className="flex flex-col items-center gap-2 bg-gray-100 dark:bg-gray-800 rounded-lg p-4 cursor-pointer w-[140px] md:w-auto"
