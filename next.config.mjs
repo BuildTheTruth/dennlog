6cfe4820a2e65b7de5e9e6c1e6cbae9cb7453a73
BuildTheTruth <dkwk1254@naver.com>
2025-05-11 21:19:48 +0900

    next image unoptimized

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    unoptimized: true,
     minimumCacheTTL: 31536000,
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    qualities: [25, 50, 75],
     formats: ['image/webp'],
     remotePatterns: [
       {
